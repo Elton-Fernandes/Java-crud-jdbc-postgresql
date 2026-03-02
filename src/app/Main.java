40ca8f101af17e02b43b78cd8a980b5479f92b6e
Elton Fernandes <eltonfernandes.ef47@gmail.com>
2026-03-02 20:43:22 -0300

    Criação de sub menus Cliente e Produto, Criação dos métodos CRUD do produto

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,9 @@
 package app;
 
 import dao.ClienteDAO;
+import dao.ProdutoDAO;
 import model.Cliente;
+import model.Produto;
 
 import java.util.List;
 import java.util.Scanner;
@@ -13,54 +15,129 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         ClienteDAO clienteDAO = new ClienteDAO();
+        ProdutoDAO produtoDAO = new ProdutoDAO();
 
-        int escolha = 1, opcao;
+        int opcao;
 
-        while (escolha != 2) {
+        do {
 
             System.out.println("==========CRUD==========");
             System.out.println("Escolha uma das opções a seguir:");
-            System.out.println("1 - Cadastrar\n2 - Listar\n3 - Alterar\n4 - Deletar\n5 - sair");
+            System.out.println("1 - Cliente\n2 - Produto\n0 - Sair");
             System.out.print("Insira a sua escolha: ");
             opcao = scanner.nextInt();
             scanner.nextLine();
+            System.out.println("========================");
 
-            if (opcao < 1 || opcao > 5) {
+            if (opcao < 0 || opcao > 2) {
                 System.out.println("Opção inválida! Tente novamente.");
                 continue;
             }
             
 
             switch (opcao) {
+
                 case 1:
-                    inserir(scanner, clienteDAO);
+                    menuCliente(scanner, clienteDAO);
                     break;
                 case 2:
-                    listar(clienteDAO);
+                    menuProduto(scanner, produtoDAO);
                     break;
-                case 3:
-                    alterar(scanner, clienteDAO);
-                    break;
-                case 4:
-                    deletar(scanner, clienteDAO);
-                    break;
-                case 5:
-                    System.out.println("FIM DO PROGRAMA!");
-                    scanner.close();
-                    return;
-
+                case 0:
+                    System.out.println("Finalizando...");
             }
-            System.out.println("Deseja realizar mais alguma ação?");
-            System.out.print("1 - Sim\n2 - Não\n");
-            System.out.print("Insira sua resposta: ");
-            escolha = scanner.nextInt();
-        }
+
+
+
+        } while (opcao != 0);
 
         scanner.close();
 
     }
 
-    public static void inserir(Scanner scanner, ClienteDAO clienteDAO) {
+    public static void menuCliente(Scanner scanner, ClienteDAO clienteDAO) {
+
+        int opcao;
+        do {
+
+            System.out.println("======MENU-CLIENTE======");
+            System.out.println("Escolha uma das opções a seguir:");
+            System.out.println("1 - Cadastrar\n2 - Listar\n3 - Alterar\n4 - Deletar\n0 - Voltar");
+            System.out.print("Insira a sua escolha: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("========================");
+
+            if (opcao < 0 || opcao > 4) {
+                System.out.println("Opção inválida! Tente novamente.");
+                continue;
+            }
+
+
+            switch (opcao) {
+                case 1:
+                    inserirCliente(scanner, clienteDAO);
+                    break;
+                case 2:
+                    listarCliente(clienteDAO);
+                    break;
+                case 3:
+                    alterarCliente(scanner, clienteDAO);
+                    break;
+                case 4:
+                    deletarCliente(scanner, clienteDAO);
+                    break;
+                case 0:
+                    System.out.println("Voltando...");
+                    break;
+
+            }
+
+        } while (opcao != 0);
+    }
+
+    public static void menuProduto(Scanner scanner, ProdutoDAO produtoDAO) {
+
+        int opcao;
+        do {
+
+            System.out.println("======MENU-PRODUTO======");
+            System.out.println("Escolha uma das opções a seguir:");
+            System.out.println("1 - Cadastrar\n2 - Listar\n3 - Alterar\n4 - Deletar\n0 - Voltar");
+            System.out.print("Insira a sua escolha: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("========================");
+
+            if (opcao < 0 || opcao > 4) {
+                System.out.println("Opção inválida! Tente novamente.");
+                continue;
+            }
+
+
+            switch (opcao) {
+                case 1:
+                    inserirProduto(scanner, produtoDAO);
+                    break;
+                case 2:
+                    listarProduto(produtoDAO);
+                    break;
+                case 3:
+                    alterarProduto(scanner, produtoDAO);
+                    break;
+                case 4:
+                    deletarProduto(scanner, produtoDAO);
+                    break;
+                case 0:
+                    System.out.println("Voltando...");
+                    break;
+
+            }
+
+        } while (opcao != 0);
+    }
+
+    public static void inserirCliente(Scanner scanner, ClienteDAO clienteDAO) {
 
         // Leitura do nome e email
         System.out.print("Insira o nome: ");
@@ -82,7 +159,7 @@ public class Main {
 
     }
 
-    public static void listar(ClienteDAO clienteDAO) {
+    public static void listarCliente(ClienteDAO clienteDAO) {
 
         // Criação do array
         List<Cliente> clientes = clienteDAO.listar();
@@ -95,7 +172,7 @@ public class Main {
         }
     }
 
-    public static void alterar(Scanner scanner, ClienteDAO clienteDAO) {
+    public static void alterarCliente(Scanner scanner, ClienteDAO clienteDAO) {
 
         // Leitura do email antigo
         System.out.print("Insira o email antigo do cliente: ");
@@ -119,7 +196,7 @@ public class Main {
 
     }
 
-    public static void deletar(Scanner scanner, ClienteDAO clienteDAO) {
+    public static void deletarCliente(Scanner scanner, ClienteDAO clienteDAO) {
 
         // Leitura do email
         System.out.print("Insira o email do cliente: ");
@@ -142,6 +219,75 @@ public class Main {
         }
 
         return email;
+    }
+
+    public static void inserirProduto(Scanner scanner, ProdutoDAO produtoDAO) {
+
+        // Leitura do nome e preço
+        System.out.print("Insira o nome do produto: ");
+        String nome = scanner.nextLine().trim();
+
+
+        System.out.print("Insira o preço: ");
+        double preco = scanner.nextDouble();
+        scanner.nextLine();
+
+        // criação do objeto produto
+        Produto produto = new Produto(nome, preco);
+
+        // obejto produtoDAO inserindo o objeto cliente no BD
+        produtoDAO.inserir(produto);
+
+        System.out.println("Produto cadastrado com sucesso!");
+
+    }
+
+    public static void listarProduto(ProdutoDAO produtoDAO) {
+
+        // Criação do array
+        List<Produto> produtos = produtoDAO.listar();
+
+        // Loop pra mostrar os nome e preços de cada produto
+        for (Produto p : produtos) {
+            System.out.println("Nome: " + p.getNome());
+            System.out.println("Preço: " + p.getPreco());
+            System.out.println("-----------------");
+        }
+    }
+
+    public static void alterarProduto(Scanner scanner, ProdutoDAO produtoDAO) {
+
+        // Leitura do nome antigo
+        System.out.print("Insira o nome do produto: ");
+        String nomeAntigo = scanner.nextLine().trim();
+
+        // Leitura dos novos dados
+        System.out.print("Insira o nome do produto: ");
+        String nome = scanner.nextLine().trim();
+
+        System.out.print("Insira o preço do produto: ");
+        double preco = scanner.nextDouble();
+        scanner.nextLine();
+
+
+
+        // Criação de um objeto cliente
+        Produto produto = new Produto(nome, preco);
+
+        // Objeto produtoDAO alterando o BD
+        produtoDAO.alterar(nomeAntigo, produto);
+
+    }
+
+    public static void deletarProduto(Scanner scanner, ProdutoDAO produtoDAO) {
+
+        // Leitura do nome
+        System.out.print("Insira o nome do produto: ");
+        String nome = scanner.nextLine().trim();
+
+        // objeto produtoDAO deletando o cliente no BD
+        produtoDAO.deletar(nome);
+
     }
 
 }
